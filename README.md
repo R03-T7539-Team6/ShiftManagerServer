bf769a737c3a2ff0c49920d03192f6b948e340ed
uai <noema01@outlook.jp>
2021-06-20 18:33:25 +0900

    add simple gin project

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # ShiftManagerServer
+> ここのディレクトリにサーバーサイドのコードをおいて、完全に分離します。
+
+シフト管理ツールのAPI
+
+
+
+## エンドポイント（仮）
+```
+GET /users/
+GET /users/{userId}
+POST /users/
+PUT /users/{userId}
+DELETE /users/{userId}
+POST /login
+```
+
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "mainmainmain",
+		})
+	})
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on localhost:8080
+}
