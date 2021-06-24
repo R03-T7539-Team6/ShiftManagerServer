8423f7ae57fc8e240b1abec76ab568041de9c05e
uai <noema01@outlook.jp>
2021-06-24 09:31:05 +0900

    create user api for test env

--- main.go
+++ main.go
@@ -2,18 +2,12 @@ package main
 
 import (
 	"github.com/R03-T7539-Team6/ShiftManagerSerer/db"
-
-	"github.com/gin-gonic/gin"
+	"github.com/R03-T7539-Team6/ShiftManagerSerer/server"
 )
 
 func main() {
-	r := gin.Default()
-
-	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello World")
-	})
-
 	db.Init()
 	defer db.Close()
-	r.Run() // listen and serve on localhost:8080
+	server.Init()
+
 }
---
+++ server/server.go
@@ -0,0 +1,28 @@
+package server
+
+import (
+	user "github.com/R03-T7539-Team6/ShiftManagerSerer/controller"
+	"github.com/gin-gonic/gin"
+)
+
+// Init is initialize server
+func Init() {
+	r := router()
+	r.Run()
+}
+
+func router() *gin.Engine {
+	r := gin.Default()
+
+	u := r.Group("/users")
+	{
+		ctrl := user.Controller{}
+		u.GET("", ctrl.Index)
+		u.GET("/:id", ctrl.Show)
+		u.POST("", ctrl.Create)
+		u.PUT("/:id", ctrl.Update)
+		u.DELETE("/:id", ctrl.Delete)
+	}
+
+	return r
+}
