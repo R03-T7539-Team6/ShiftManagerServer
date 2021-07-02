65a6594c50286081d32bd7bff74e8fe2c0ab6df1
uai <noema01@outlook.jp>
2021-07-03 03:00:46 +0900

    arrange DB setting and add .gitignore

--- db/db.go
+++ db/db.go
@@ -9,7 +9,12 @@ entityを追加するたびに、autoMigrationに追記すると
 package db
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
 
 	// Use PostgresSQL in gorm
 	_ "github.com/jinzhu/gorm/dialects/postgres"
@@ -22,14 +27,28 @@ var (
 
 // Init is initialize db from main function
 func Init(isReset bool, models ...interface{}) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		os.Exit(1)
+	}
 
-	db, err = gorm.Open(
-		"postgres",
-		"host=172.28.48.1 port=5432 user=gorm dbname=gorm password=gorm sslmode=disable",
-	)
+	db_name := os.Getenv("DB_NAME")
+	db_host := os.Getenv("DB_HOST")
+	db_port := os.Getenv("DB_PORT")
+	db_user := os.Getenv("DB_USER")
+	db_pass := os.Getenv("DB_PASS")
+	db_timezone := os.Getenv("DB_TIMEZONE")
+	db_sslmode := os.Getenv("DB_SSLMODE")
+
+	url := fmt.Sprintf("host=%s port=%s	dbname=%s user=%s password=%s TimeZone=%s sslmode=%s",
+		db_host, db_port, db_name, db_user, db_pass, db_timezone, db_sslmode)
+
+	db, err = gorm.Open("postgres", url)
 	if err != nil {
 		panic(err)
 	}
+
 	// テーブル名の複数形化を無効にする
 	db.SingularTable(true)
 
