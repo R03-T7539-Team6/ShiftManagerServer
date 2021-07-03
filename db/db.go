fb9dd6c22ac7fa980d0dee112a062de613ba4189
uai <noema01@outlook.jp>
2021-07-04 07:44:34 +0900

    db setting change to heroku DATABASE_URL

--- db/db.go
+++ db/db.go
@@ -1,12 +1,10 @@
 package db
 
 import (
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/jinzhu/gorm"
-	"github.com/joho/godotenv"
 
 	// Use PostgresSQL in gorm
 	_ "github.com/jinzhu/gorm/dialects/postgres"
@@ -19,24 +17,25 @@ var (
 
 // Init is initialize db from main function
 func Init(isReset bool, models ...interface{}) {
-	err := godotenv.Load(".env")
+	// err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 		os.Exit(1)
 	}
 
-	db_name := os.Getenv("DB_NAME")
-	db_host := os.Getenv("DB_HOST")
-	db_port := os.Getenv("DB_PORT")
-	db_user := os.Getenv("DB_USER")
-	db_pass := os.Getenv("DB_PASS")
+	databaseUrl := os.Getenv("DATABASE_URL")
+	// db_name := os.Getenv("DB_NAME")
+	// db_host := os.Getenv("DB_HOST")
+	// db_port := os.Getenv("DB_PORT")
+	// db_user := os.Getenv("DB_USER")
+	// db_pass := os.Getenv("DB_PASS")
 	// db_timezone := os.Getenv("DB_TIMEZONE")
 	// db_sslmode := os.Getenv("DB_SSLMODE")
 
-	url := fmt.Sprintf("host=%s port=%s	dbname=%s user=%s password=%s",
-		db_host, db_port, db_name, db_user, db_pass)
+	// url := fmt.Sprintf("host=%s port=%s	dbname=%s user=%s password=%s",
+	// 	db_host, db_port, db_name, db_user, db_pass)
 
-	db, err = gorm.Open("postgres", url)
+	db, err = gorm.Open("postgres", databaseUrl)
 	if err != nil {
 		panic(err)
 	}
